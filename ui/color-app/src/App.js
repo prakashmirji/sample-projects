dbf7363152637ae8a40f73a10fd502020fb7cb56
prakashmirji <prakash-r.mirji@hpe.com>
2020-09-15 14:42:18 +0530

    fixed alignment

--- ui/color-app/src/App.js
+++ ui/color-app/src/App.js
@@ -1,25 +1,25 @@
 import ReactDOM from 'react-dom';
-import React, {useState, useRef}  from 'react';
+import React, { useState, useRef } from 'react';
 
 const App = props => {
-	const [color, setColor] = useState('black')
+  const [color, setColor] = useState('black')
   const inputRef = useRef(null)
 
   const handleButtonClick = () => {
-      setColor(inputRef.current.value || 'black')
+    setColor(inputRef.current.value || 'black')
   }
 
   return (
-  	<div id="container">
-      <div id="circle" style={{backgroundColor: color}} />
-  	  <input type="text" ref={inputRef} />
+    <div id="container">
+      <div id="circle" style={{ backgroundColor: color }} />
+      <input type="text" ref={inputRef} />
       <input type="button" onClick={handleButtonClick} value="Change Circle Color" />
-  	</div>
+    </div>
   )
 }
 
 ReactDOM.render(
-  <App />, 
+  <App />,
   document.getElementById('root'),
 );
 
